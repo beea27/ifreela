2b9a28a351c68d0d723cfb03e7b3c785c22fc810
LidiaEstepfanie <lidia.lidia789@hotmail.com>
2020-12-05 19:30:37 -0300

    Botão Whatsapp

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
+
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor(public platform: Platform) {}
+
+  chat() {
+    var whats;
+
+    this.platform.ready().then(() => {
+      whats.plugins.Whatsapp.send('+5511962700772');
+    });
+  }
+
 
   ngOnInit() {}
 
